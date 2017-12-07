af09b075b9009f28960f186de41a295b0e6b0979
Simon Svensson <simon.svensson@lund.ntig.se>
2017-12-07 14:09:52 +0100

    Fixed button and added bounding rectangle to all BaseUiComponents

--- MonoForms/MonoFormsLibrary/UI/UiButton.cs
+++ MonoForms/MonoFormsLibrary/UI/UiButton.cs
@@ -7,8 +7,11 @@ namespace MonoFormsLibrary.UI
     public class UiButton : BaseUiComponent
     {
         #region Private fields
-        private Texture2D _texture;
+        public Texture2D _texture;
         private Texture2D _highlightTexture;
+        public float textureScale = 2f;
+        public float textureWidth;
+        public float textureHeight;
         #endregion
 
         #region Public constructors
@@ -16,6 +19,9 @@ namespace MonoFormsLibrary.UI
         {
             _texture = Game.Content.Load<Texture2D>("button");
             //_highlightTexture = Game.Content.Load<Texture2D>("playerLife2_red");
+            textureWidth = _texture.Width * textureScale;
+            textureHeight = _texture.Height * textureScale;
+            BoundsRectangle = new Rectangle((int)(Position.X - (_texture.Width * textureScale) / 2), (int)(Position.Y - (_texture.Height * textureScale) / 2), _texture.Width, _texture.Height);
         }
         #endregion
 
@@ -25,12 +31,12 @@ namespace MonoFormsLibrary.UI
         public override void Draw(SpriteBatch spriteBatch)
         {
             Vector2 textSize = Font.MeasureString(Text);
-            spriteBatch.Draw(_texture, Position - new Vector2(_texture.Width / 2f, _texture.Height / 2f), null, Color.White, 0f , new Vector2(_texture.Width / 2f, _texture.Height / 2f), 0.25f, SpriteEffects.None, 0f);
-            spriteBatch.DrawString(Font, Text, Position - textSize / 2, Color.Black, 0, Vector2.Zero, 1f, SpriteEffects.None, 0);
+            spriteBatch.Draw(_texture, Position - new Vector2(textureWidth / 2f, textureHeight / 2f), null, Color.White, 0f, new Vector2(), textureScale, SpriteEffects.None, 0f);
+            spriteBatch.DrawString(Font, Text, Position - textSize / 2 - new Vector2(45 * textureScale, 0), Color.White, 0, Vector2.Zero, 1f, SpriteEffects.None, 0);
 
             if (IsHighlighted)
                 spriteBatch.Draw(_highlightTexture, Position - new Vector2(_highlightTexture.Width, _highlightTexture.Height / 2f) - new Vector2(textSize.X / 2, 0), Color.White);
-        } 
+        }
         #endregion
     }
 }
